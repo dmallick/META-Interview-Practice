4578c7f7e65e9b2408d9f07cdef95f7d29f38d6d
Debasish Mallick <debasish.mallick@gmail.com>
2025-12-06 16:36:19 +0530

    Implemented if the List has a cycle

--- MetaPractice/src/ListNode.java
+++ MetaPractice/src/ListNode.java
@@ -1,63 +1,95 @@
 public class ListNode {
-      int val;
-      ListNode next;
-      ListNode() {}
-      ListNode(int val) { this.val = val; }
-      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  int val;
+  ListNode next;
 
-      // Reverse a linkedList
-      public ListNode reverseListNode(ListNode head){
-        // 1 2 3 4 5
-        ListNode prev = null;
-        ListNode current = head;
+  ListNode() {
+  }
 
-        while (current != null) {           // current =1
-            ListNode nexNode = current.next;
-            
-            current.next = prev; //actual reversing
-            prev = current; // mov prev to curr
-            current = nexNode; // mov cur to next
+  ListNode(int val) {
+    this.val = val;
+  }
+
+  ListNode(int val, ListNode next) {
+    this.val = val;
+    this.next = next;
+  }
+
+  // Reverse a linkedList
+  public ListNode reverseListNode(ListNode head) {
+    // 1 2 3 4 5
+    ListNode prev = null;
+    ListNode current = head;
+
+    while (current != null) { // current =1
+      ListNode nexNode = current.next;
+
+      current.next = prev; // actual reversing
+      prev = current; // mov prev to curr
+      current = nexNode; // mov cur to next
+    }
+    return prev;
+  }
+
+  // Finding Middle of LinkedList
+  public ListNode findMiddle(ListNode head) {
+    ListNode fast = head;
+    ListNode slow = head;
+
+    while (fast != null && fast.next != null) {
+      fast = fast.next.next;
+      slow = slow.next;
+    }
+    return slow;
+  }
+
+  // Finding the CYcle
+public boolean hasCycle(ListNode head) {
+      if (head == null) return false;
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;         // move 1 step
+            fast = fast.next.next;    // move 2 steps
+
+            if (slow == fast) {       // pointers meet → cycle exists
+                return true;
+            }
         }
-        return prev;
-      }
 
-      //Finding Middle of LinkedList
-        public ListNode findMiddle(ListNode head){
-          ListNode fast = head;
-          ListNode slow = head;
-
-          while (fast != null && fast.next != null) {
-              fast = fast.next.next;
-              slow = slow.next;  
-          }
-          return slow;
-        }
-
-
-          public static void main(String[] args) {
-        // Create linked list: 1 -> 2 -> 3 -> 4 -> 5
-        ListNode head = new ListNode(1);
-        head.next = new ListNode(2);
-        head.next.next = new ListNode(3);
-        head.next.next.next = new ListNode(4);
-        head.next.next.next.next = new ListNode(5);
-
-        ListNode sol = new ListNode();
-        
-        //uncomment below for testing reversal    
-        //ListNode reversed = sol.reverseListNode(head);
-        //printList(reversed);
-        
-        // Find the middle    
-        ListNode middle = sol.findMiddle(head);
-        System.out.println(middle.val);
-     
+      return false;  // fast reached null → no cycle
     }
 
-    public static void printList(ListNode head) {
-        while (head != null) {
-            System.out.print(head.val + " ");
-            head = head.next;
-        }
+  public static void main(String[] args) {
+    // Create linked list: 1 -> 2 -> 3 -> 4 -> 5
+    ListNode head = new ListNode(1);
+    head.next = new ListNode(2);
+    head.next.next = new ListNode(3);
+    head.next.next.next = new ListNode(4);
+    head.next.next.next.next = new ListNode(5);
+    //uncomment it to test the cycle
+    head.next.next.next.next.next = head.next.next;
+    ListNode sol = new ListNode();
+
+    // uncomment below for testing reversal
+    // ListNode reversed = sol.reverseListNode(head);
+    // printList(reversed);
+
+    // uncomment below for testing the middle of LL
+    //ListNode middle = sol.findMiddle(head);
+    //System.out.println(middle.val);
+
+
+    //uncomment below for testing Cycle
+     System.out.println(sol.hasCycle(head));
+
+  }
+
+  public static void printList(ListNode head) {
+    while (head != null) {
+      System.out.print(head.val + " ");
+      head = head.next;
     }
   }
+}
